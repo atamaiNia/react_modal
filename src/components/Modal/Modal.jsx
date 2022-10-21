cd80dfb58149bda5ff3ccc8382a0b4d1667e9489
atamaiNia <bimatideka@gmail.com>
2022-10-21 14:12:35 +0300

    completed

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -8,22 +8,34 @@ export default class Modal extends Component {
   componentDidMount() {
     console.log('Modal componentDidMount');
 
-    window.addEventListener('keydown', e => {
-      if (e.code === 'Escape') {
-        console.log(`Натиснули ${e.code}, необхідно закрити модалку`);
-
-        this.props.onClose();
-      }
-    });
+    window.addEventListener('keydown', this.handleKeyDown);
   }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      console.log(`Натиснули ${e.code}, необхідно закрити модалку`);
+
+      this.props.onClose();
+    }
+  };
 
   componentWillUnmount() {
     console.log('Modal componentWillUnmount');
+
+    window.removeEventListener('keydown', this.handleKeyDown);
   }
+
+  handleBackdropClick = e => {
+    if (e.currentTarget === e.target) {
+      // e.currentTarget - вспливає до того елемента на якому повішаний слухач
+      // e.target - показує на якому елементі клікнули
+      this.props.onClose();
+    }
+  };
 
   render() {
     return createPortal(
-      <Backdrop>
+      <Backdrop onClick={this.handleBackdropClick}>
         <Content>{this.props.children}</Content>
       </Backdrop>,
       modalRoot
